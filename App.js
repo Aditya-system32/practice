dfbef7803217c981f1dd626615fbae8d07cc1c79
Aditya Yadav <skilladi123@gmail.com>
2025-06-20 02:22:07 +0530

    update

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
-
-const heading1 = React.createElement("h1",{},"hello")
-
-const heading2 = React.createElement("h2",{},"heading2")
-
-const container = React.createElement("div",{},[heading1,heading2])
+import "/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(container)
+const Headers = () => {
+    return(
+        <div className = "container">
+            <img className="logo" src ="https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg" />
+            <input type = "text" placeholder="Search Bar" className = "search-box"/>
+            <img className="user-icon" src ="https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg" />
+        </div>
+    )
+}
+
+root.render(<Headers />)
 
 
 
